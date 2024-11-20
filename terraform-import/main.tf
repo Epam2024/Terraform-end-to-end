4b7117c7b8d1edebc3ee6ccaaa1c1c99287765e0
Epam2024 <soumyadip_chatterjee@epam.com>
2024-11-20 09:01:46 +0000

    added import stage

--- terraform-import/main.tf
+++ terraform-import/main.tf
@@ -1,3 +1,12 @@
-resource "aws_instance" "terraform-import" {
+resource "aws_instance" "instance1" {
     instance_type = "t2.micro"
+    ami = "ami-0866a3c8686eaeeba"
+    
 }
+resource "aws_instance" "instance2" {
+    instance_type = "t2.micro"
+    ami = "ami-0866a3c8686eaeeba"
+    
+}
+
+
